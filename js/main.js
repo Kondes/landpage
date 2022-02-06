d6e3328075359ecb17ccaac8a9b03242b7f6cb15
Andrii Kondysiuk <kondes@Andriis-MacBook-Pro.local>
2022-02-06 18:01:31 +0200

    backdrop-menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,48 @@
 let menuBtn = document.querySelector('.menu__btn');
 let menuBurg = document.querySelector('.menu__burger')
+let intervalId;
+// document.querySelectorAll('.selectField').forEach((el) => {
+//     el.addEventListener('click', () => {
+//         let content = el.nextElementSibling
+//         if (content.style.display) {
+//             document.querySelectorAll('.content').forEach((el) => el.style.display = null)
+//         } else {
+//             document.querySelectorAll('.content').forEach((el) => el.style.display = null)
+//             content.style.display = content.display + 'block'
+//         }
+//     })
+// })
+
+document.querySelectorAll('.selectField').forEach(e => {
+    e.addEventListener('click', e => {
+        const menu = e.currentTarget.dataset.path
+        document.querySelectorAll('.hide').forEach(e => {
+            if (!document.querySelector(`[data-target=${menu}]`).classList.contains('open')) {
+                document.querySelector(`[data-target=${menu}]`).classList.add('menu-active')
+              intervalId =  setTimeout(() => {
+                  document.querySelector(`[data-target=${menu}]`).classList.add('open');
+                },0)
+            }
+            if (document.querySelector(`[data-target=${menu}]`).classList.contains('open')) {
+                clearTimeout(intervalId)
+                document.querySelector(`[data-target=${menu}]`).classList.remove('menu-active')
+                intervalId = setTimeout(() => {
+                    document.querySelector(`[data-target=${menu}]`).classList.remove('open')
+                }, 0)
+            }
+        })
+    })
+})
+// const selecttext = document.getElementById('selecttext')
+const serviceMenu = document.querySelectorAll('.serviceMenu')
 
 menuBtn.addEventListener('click', function () {
     menuBtn.classList.toggle('active');
     menuBurg.classList.toggle('active')
 })
+
+// selectField.addEventListener('click', function () {
+    
+//     serviceMenu.classList.toggle('hide')
+// })
+    
