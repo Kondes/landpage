a59eaa4587a05a050a64894d8fd913deb948f75e
Andrii Kondysiuk <kondes@Andriis-MacBook-Pro.local>
2022-02-09 03:55:08 +0200

    update

--- js/slider.js
+++ js/slider.js
@@ -1,36 +1,42 @@
-const imgs = document.querySelectorAll(".example__slider .example__slider-line img");
+const imgs = document.querySelectorAll(
+  ".example__slider .example__slider-line img"
+);
 const exmpSliderLine = document.querySelector(".example__slider-line");
 let counts = 0;
 let widths;
 
 function initial() {
-    widths = document.querySelector(".example__slider").offsetWidth;
-    exmpSliderLine.style.widths = widths * imgs.length + "px";
-    imgs.forEach((item) => {
-        item.style.widths = widths + "px";
-        item.style.height = "auto";
-    });
-    rollSliders();
+  widths = document.querySelector(".example__slider").offsetWidth;
+  exmpSliderLine.style.widths = widths * imgs.length + "px";
+  imgs.forEach((item) => {
+    item.style.widths = widths + "px";
+    item.style.height = "auto";
+  });
+  rollSliders();
 }
 window.addEventListener("resize", initial);
 initial();
 
-document.querySelector(".example__slider-prev").addEventListener("click", function () {
+document
+  .querySelector(".example__slider-prev")
+  .addEventListener("click", function () {
     counts--;
     if (counts < 0) {
-        counts = imgs.length - 1;
+      counts = imgs.length - 1;
     }
     rollSliders();
-});
+  });
 
-document.querySelector(".example__slider-next").addEventListener("click", function () {
+document
+  .querySelector(".example__slider-next")
+  .addEventListener("click", function () {
     counts++;
     if (counts >= imgs.length) {
-        counts = 0;
+      counts = 0;
     }
     rollSliders();
-});
+  });
 
 function rollSliders() {
-    exmpSliderLine.style.transform = "translate(-" + counts * widths + "px)";
+  exmpSliderLine.style.transform = "translate(-" + counts * widths + "px)";
 }
